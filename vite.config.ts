e5eb32aed5014b026441b65fbfb0614fb012f3b3
Nelson Fernandes <nelson.fernandes@fanduel.com>
2026-04-03 09:48:14 +0100

    add vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,19 @@
-import { defineConfig } from 'vitest/config'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
+import { loadEnv } from "vite";
 
-export default defineConfig({
-  plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/test-setup.ts',
-  },
-})
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [react()],
+    define: {
+      VITE_API_KEY: JSON.stringify(env.VITE_API_KEY),
+    },
+    test: {
+      globals: true,
+      environment: "jsdom",
+      setupFiles: "./src/test-setup.ts",
+    },
+  };
+});
